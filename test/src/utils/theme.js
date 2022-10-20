4c218004a3361b3513c7d5e4a8c882a6b8e4c8d7
Diego Lima <xxdiegolsxx@gmail.com>
2022-10-20 00:02:50 -0300

    theme

---
+++ test/src/utils/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    primaryColor: '#3E77DE',
+    secondaryColor: '#33CADE',
+    thirdColor: '#DEA01D',
+    green: '#28DE8D',
+    red: '#DE5E49',
+    white: '#FFFAFA',
+    black: '#121212',
+  },
+};
